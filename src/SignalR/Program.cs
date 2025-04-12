b2f3722d68074f6e3a4508fc1629b7583af7ae6e
paolo.fullone <paolo.enrico@gmail.com>
2025-04-12 11:34:09 -0300

    update DI

--- src/SignalR/Program.cs
+++ src/SignalR/Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IDbConnectionFactory>(sb =>
     new DbConnectionFactory(builder.Configuration.GetConnectionString("SqlServer")?.Trim()!));
 
-builder.Services.AddSingleton<ISampleMessageService, SampleMessageService>();
-builder.Services.AddTransient<ISampleMessageRepository, SampleMessageRepository>();
+builder.Services.AddScoped<ISampleMessageService, SampleMessageService>();
+builder.Services.AddScoped<ISampleMessageRepository, SampleMessageRepository>();
 
 builder.Services.AddSignalR();
 
